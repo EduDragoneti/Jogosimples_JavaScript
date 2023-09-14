af399f2b895165294a4ab6e4fda81587a32fc7f2
EduDragoneti <143272519+EduDragoneti@users.noreply.github.com>
2023-09-13 21:56:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const score = document.querySelector('.score');
+let count = 0;
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+
+        mario.classList.remove('jump');
+
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    console.log('loop')
+    
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    console.log(marioPosition);
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = 'imgs/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        alert(`Game Over! Seu score foi: ${count}`);
+        count = 0;
+
+        clearInterval(loop);
+
+    }
+
+    count++;
+    score.innerHTML = `SCORE: ${count}`;
+
+}, 10);
+
+document.addEventListener('keydown', jump);
